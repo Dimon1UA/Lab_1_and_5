ac657dc6a83c51d3a7540e60614ad5947dbe307d
Dimon1UA <92036555+Dimon1UA@users.noreply.github.com>
2024-11-16 01:22:26 +0200

    Add files via upload

---
+++ Lab_1_pr/Lab_1_pr/Lab_1_pr.cpp
@@ -0,0 +1,40 @@
+#include <Windows.h>
+using namespace System;
+/*
+int main(array<System::String^>^ args)
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	String^ inputString = L"Продукти в АТБ: молоко, хліб, картопля, яйця.";
+		Console::WriteLine(L"Початковий рядок: {0}", inputString);
+	int colonPosition = inputString->IndexOf(L':');
+	if (colonPosition != -1) {
+		String^ result = inputString->Substring(0, colonPosition);
+		Console::WriteLine(L"Рядок до першої двокрапки: {0}", result);
+	}
+	else {
+		Console::WriteLine(L"Двокрапка не знайдена.");
+	}
+
+	return 0;
+}
+*/
+
+/*
+int main(array<System::String^>^ args)
+{
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
+String^ inputString = L"Тестовий, рядок, з, комами.";
+Console::WriteLine(L"Початковий рядок: {0}", inputString);
+int lastCommaPosition = inputString->LastIndexOf(L',');
+if (lastCommaPosition != -1) {
+Console::WriteLine(L"Позиція останньої коми: {0}",
+lastCommaPosition + 1);
+}
+else {
+Console::WriteLine(L"Кома не знайдена.");
+}
+return 0;
+}
+*/
